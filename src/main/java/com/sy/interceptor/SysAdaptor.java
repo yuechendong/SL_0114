660a7a5f8cfc11abfa8452c7ed742b9b14e48221
yuechendong <1072876302@qq.com>
2019-06-28 19:33:42 +0800

    tijiao

--- src/main/java/com/sy/interceptor/SysAdaptor.java
+++ src/main/java/com/sy/interceptor/SysAdaptor.java
@@ -1,6 +1,5 @@
 package com.sy.interceptor;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
